1d7a189832ad7bb7e244e1f01dd800538665275e
Gaston Sanchez <gaston.stat@gmail.com>
2013-06-03 20:18:54 +0200

    Renamed turnlist into turner

--- R/from_to.r
+++ R/from_to.r
@@ -1,10 +1,8 @@
-#' @title Start and ending positions of the elements in a list
-#' 
-#' @description
 #' Get the starting position 'from' and the ending position 'to' of the 
-#' elements contained in a list
+#' elements contained in a vector (or a list of vectors)
 #'
-#' @param alist a list (preferably of vectors)
+#' @param x a numeric vector or a list of vectors
+#' @param ... further arguments are ignored
 #' @return A list with two vectors: '$from' and '$to'.
 #' '$from' contains the indices with starting positions.
 #' '$to' contains the indices with ending positions.
@@ -12,25 +10,46 @@
 #' @seealso \code{\link{lengths}}, \code{\link{listsize}}
 #' @export
 #' @examples
-#' # let's say you have a list like this
+#' # let's say you have a numeric vector like this
+#' num_vec = c(2, 3, 1, 4)
+#' 
+#' # get 'from' and 'to' indices
+#' start_end = from_to(num_vec)
+#' from = start_end$from
+#' to = start_end$to
+#' 
+#' #' let's say you have a list like this
 #' str_list = list(c("a","b","c"), c("d", "e"), c("f","g","h"))
 #' 
 #' # get 'from' and 'to' indices
 #' start_end = from_to(str_list)
 #' from = start_end$from
 #' to = start_end$to
-#' 
-#' # now let's convert str_list into a vector
-#' str_vec = unlist(str_list)
-#' 
-#' # recover first set of letters on the string vector
-#' str_vec[from[1]:to[1]]
-#' 
-#' # recover third set of letters on the string vector
-#' str_vec[from[3]:to[3]]
-from_to <- function(alist)
+from_to <- function(x, ...) {
+  UseMethod("from_to", x)  
+}
+
+
+#' @S3method from_to numeric
+from_to.numeric <- function(x, ...)
 {
-  aux = lengths(alist)
+  if (!is.vector(x) || !is.numeric(x))
+    stop("\nA numeric vector is required")
+  
+  to = cumsum(x)
+  from = to - x + 1
+  list(from=from, to=to)
+}
+
+
+#' @S3method from_to list
+from_to.list <- function(x, ...)
+{
+  all_vectors = unlist(lapply(x, is.vector))
+  if (length(x) != sum(all_vectors))
+    stop("\nA list of vectors is required")
+  
+  aux = unlist(lapply(x, length))
   to = cumsum(aux)
   from = to - aux + 1
   list(from=from, to=to)
--- R/indexify.r
+++ R/indexify.r
@@ -1,15 +1,22 @@
-#' @title Create indices for elements in a list
+#' @title Create indices for elements in a vector or list
 #' 
 #' @description
 #' Create indexed components for the elements of a list.
 #'
-#' @param alist a list (preferably of vectors)
-#' @param out string indicating the output format (\code{"vector"} or \code{"list"})
-#' @return A vector or list indexed numbers
+#' @param x a numeric vector or list of vectors
+#' @param out string indicating the output format 
+#' (\code{"vector"} or \code{"list"})
+#' @return A vector (or list) of indexed numbers
 #' @author Gaston Sanchez
 #' @seealso \code{\link{listify}}
 #' @export
 #' @examples
+#' # let's say you have a numeric vector like this
+#' num_vec = c(2, 3, 1, 4)
+#' 
+#' # get indices in vector format
+#' indexify(num_vec)
+#' 
 #' # let's say you have a list like this
 #' str_list = list(c("a","b","c"), c("d", "e"), c("f","g","h"))
 #' 
@@ -18,9 +25,31 @@
 #' 
 #' # get indices in list format
 #' indexify(str_list, "list")
-indexify <- function(alist, out = "vector")
+indexify <- function(x, out) {
+  UseMethod("indexify", x)  
+}
+
+
+#' @S3method indexify numeric
+indexify.numeric <- function(x, out = "vector")
 {
-  aux = lengths(alist)
+  if (!is.vector(x) || !is.numeric(x))
+    stop("\nA numeric vector is required")
+  
+  if (out == "vector")
+    rep(seq_along(x), x)
+  else mapply(rep, seq_along(x), x)
+}
+
+
+#' @S3method indexify list
+indexify.list <- function(x, out = "vector")
+{
+  all_vectors = unlist(lapply(x, is.vector))
+  if (length(x) != sum(all_vectors))
+    stop("\nA list of vectors is required")
+  
+  aux = unlist(lapply(x, length))
   if (out == "vector")
     rep(seq_along(aux), aux)
   else mapply(rep, seq_along(aux), aux)
