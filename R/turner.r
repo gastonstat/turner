4bb0ffff7c259cb69b91fa3107b5273bcb903db4
Gaston Sanchez <gaston.stat@gmail.com>
2015-08-28 14:46:48 -0700

    version 0.1.8

---
+++ R/turner.r
@@ -0,0 +1,9 @@
+#' \pkg{turner} Turns vectors and lists of vectors into indexed structures
+#'
+#' Package designed for working with vectors and lists of vectors,
+#' mainly for turning them into other indexed data structures.
+#'
+#' @name turner
+#' @docType package
+#' @import tester
+NULL
